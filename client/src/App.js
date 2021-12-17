6ec509cc287b795d539abc193e82c89fb3d54be8
Akari Bowens <akarimbowens@gmail.com>
2021-12-16 23:51:12 -0800

    Put api request into Quote component, added css to quote component

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Homepage from "./pages/Homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 
 import Axios from "axios";
+import Quote from "./components/Quote";
 
 function App() {
-  const [quote, setQuote] = useState("");
+  // const [quote, setQuote] = useState("");
 
-  const getQuote = () => {
-    // change id every time
-    var options = {
-      method: "GET",
-      url: "https://healthruwords.p.rapidapi.com/v1/quotes/",
-      params: { t: "Wisdom", maxR: "3", size: "medium", id: "731" },
-      headers: {
-        "x-rapidapi-host": "healthruwords.p.rapidapi.com",
-        "x-rapidapi-key": "26f54531bfmshdda1bdc35d9c8e8p17cd41jsn144a6b2d2d77",
-      },
-    };
+  // useEffect(() => {
+  //   getQuote();
+  // }, []);
 
-    Axios.request(options)
-      .then(function (response) {
-        console.log(response.data);
-        setQuote(response.data.media);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
+  // const getQuote = () => {
+  //   // change id every time
+  //   var options = {
+  //     method: "GET",
+  //     url: "https://healthruwords.p.rapidapi.com/v1/quotes/",
+  //     params: { t: "Wisdom", maxR: "3", size: "medium", id: "731" },
+  //     headers: {
+  //       "x-rapidapi-host": "healthruwords.p.rapidapi.com",
+  //       "x-rapidapi-key": "26f54531bfmshdda1bdc35d9c8e8p17cd41jsn144a6b2d2d77",
+  //     },
+  //   };
+
+  //   Axios.request(options)
+  //     .then(function (response) {
+  //       console.log(response.data);
+  //       setQuote(response.data.media);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // };
 
   return (
-    // <BrowserRouter>
-    //   <Header />
-    //   <Routes>
-    //     <Route path="/" element={<Homepage />} />
-    //     <Route path="/quote-generator" element={<QuoteGenerator />} />
-    //     <Route path="/quote-list" element={<QuoteList />} />
-    //   </Routes>
-    // </BrowserRouter>
-
-    <div>
+    <div className="app">
       <h1>Hello</h1>
-      <button onClick={getQuote}>Get Random Quote</button>
-      <img src={quote} alt="" />
+      <Quote />;
+      {/* <button onClick={getQuote}>Get Random Quote</button>
+      <img src={quote} alt="" /> */}
     </div>
   );
 }
--- client/src/components/Quote.js
+++ client/src/components/Quote.js
@@ -1,23 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Quote.css";
+import Axios from "axios";
 
 function Quote() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = () => {
+    // change id every time
+    var options = {
+      method: "GET",
+      url: "https://healthruwords.p.rapidapi.com/v1/quotes/",
+      params: { t: "Wisdom", maxR: "3", size: "medium", id: "731" },
+      headers: {
+        "x-rapidapi-host": "healthruwords.p.rapidapi.com",
+        "x-rapidapi-key": "26f54531bfmshdda1bdc35d9c8e8p17cd41jsn144a6b2d2d77",
+      },
+    };
+
+    Axios.request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setQuote(response.data.media);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="quote-section">
-      <div className="quote-section__display">
-        {/* window  */}
-        {/* <p>{newResponse}</p> */}
+    <div className="quote">
+      <h1>Random Quote</h1>
+
+      <div className="quote--image__container">
+        <img className="quote--image" src={quote} alt="" />
       </div>
-      <div className="quote-section__categories">
-        {/* dropdown_default-random menu */}
-        {/* form dropdown setamount */}
-        {/* event handler onClick for category and amount */}
-      </div>
-      <div className="quote-section__generate">
-        {/*amount -- button element */}
+      <div className="quote--button__container">
+        <button className="quote--button" onClick={getQuote}>
+          Get Random Quote
+        </button>
       </div>
     </div>
   );
+  {
+    /* <div className="quote-section">
+      <div className="quote-section__display"> */
+  }
+  {
+    /* window  */
+  }
+  {
+    /* <p>{newResponse}</p> */
+  }
+  // </div>
+  // <div className="quote-section__categories">
+  {
+    /* dropdown_default-random menu */
+  }
+  {
+    /* form dropdown setamount */
+  }
+  {
+    /* event handler onClick for category and amount */
+  }
+  // </div>
+  {
+    /* <div className="quote-section__generate"> */
+  }
+  {
+    /*amount -- button element */
+  }
+  // </div>
+  // </div>
 }
 
 export default Quote;
